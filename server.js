1105f0aa5936193dacbbfe28d0c02622427fcaa6
Prateek Mishra <prateek.m046@gmail.com>
2019-07-31 03:44:14 +0530

    chore: Update project for deployment to Heroku

--- server.js
+++ server.js
@@ -2,8 +2,8 @@ const express = require('express');
 const app = express();
 const path = require('path');
 
-app.use(express.static(__dirname + '/dist'));
-app.listen(process.env.PORT || 8080);
+app.use(express.static(__dirname + '/dist/reddit-prototype-angular'));
 app.get('/*', (req, res) => {
-    res.sendFile(path.join(__dirname + '/dist/index.html'));
+    res.sendFile(path.join(__dirname + '/dist/reddit-prototype-angular/index.html'));
 });
+app.listen(process.env.PORT || 8080);
