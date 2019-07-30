a90f1a17bda0e5dbcd43458d8854c20b6c2b63a2
Prateek Mishra <prateek.m046@gmail.com>
2019-07-31 02:49:19 +0530

    feat: Add details component

---
+++ src/components/details/details.module.ts
@@ -0,0 +1,19 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { DetailsComponent } from './details.component';
+
+const routes: Routes = [{
+    path: '', component: DetailsComponent
+}];
+
+@NgModule({
+    declarations: [DetailsComponent],
+    imports: [
+        RouterModule.forChild(routes)
+    ],
+    exports: [RouterModule]
+})
+export class DetailsComponentModule {
+    constructor() {}
+}
